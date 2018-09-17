2644d7552838a0a577abed567601915bbd7f7833
Jakub Tober <jakub.tober@gmail.com>
2018-09-17 20:18:08 +0200

    Custom django forms added

--- climbing_planner/planner_app/forms.py
+++ climbing_planner/planner_app/forms.py
@@ -3,5 +3,43 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=64, label="")
     password = forms.CharField(max_length=64, widget=forms.PasswordInput, label="")
-    username.widget.attrs.update({'placeholder': 'username'})
-    password.widget.attrs.update({'placeholder': 'password'})
+
+    username.widget.attrs.update({
+        'placeholder': 'username',
+        'class':'validate',
+        'data-length': '64',
+    })
+    password.widget.attrs.update({
+        'placeholder': 'password',
+        'class': 'validate',
+    })
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=64, label="")
+    email = forms.EmailField()
+    password = forms.CharField(max_length=64, widget=forms.PasswordInput, label="")
+    is_instructor = forms.BooleanField(required=False)
+
+    username.widget.attrs.update({
+        'placeholder': 'username',
+        'class':'validate',
+        'data-length': '64',
+    })
+    email.widget.attrs.update({
+        'placeholder': 'email',
+        'class':'validate',
+        'data-length': '64',
+    })
+    password.widget.attrs.update({
+        'placeholder': 'password',
+        'class': 'validate',
+    })
+
+class ResetPasswordForm(forms.Form):
+    email = forms.EmailField()
+
+    email.widget.attrs.update({
+        'placeholder': 'email',
+        'class':'validate',
+        'data-length': '64',
+    })
--- climbing_planner/planner_app/views.py
+++ climbing_planner/planner_app/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.views import View
+from planner_app.forms import (
+    LoginForm,
+    RegisterForm,
+    ResetPasswordForm
+)
 
 # Create your views here.
 
@@ -7,20 +13,47 @@ class WelcomeView(View):
     def get(self, request):
         return render(request, 'welcome.html')
 
+
 class LoginView(View):
     def get(self, request):
-        return render(request, 'login.html')
+        ctx = {
+            'form': LoginForm,
+        }
+        return render(request, 'login.html', ctx)
 
     def post(self, request):
-        pass
+        form = LoginForm(request.POST)
+        ctx = {
+            'form': form,
+        }
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+            else:
+                form.add_error(field=None, error="Please enter correct credentials")
+                return render(request, 'login.html', ctx)
+
+        return render(request, 'home.html', ctx)
+
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'register.html')
+        ctx = {
+            'form': RegisterForm,
+        }
+        return render(request, 'register.html', ctx)
 
     def post(self, request):
         pass
 
+
 class ResetPasswordView(View):
     def get(self, request):
-        return render(request, 'reset-password.html')
+        ctx = {
+            'form': ResetPasswordForm,
+        }
+        return render(request, 'reset-password.html', ctx)
